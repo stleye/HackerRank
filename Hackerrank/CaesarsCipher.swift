f84ab4288c51934d2cdfe2c663d8cdabbf05c0e0
Sebastian Tleye <stleye@gmail.com>
2024-01-19 22:33:17 -0300

    Added Hackerrank/CaesarsCipher.swift and Python/findZigZangSequence.py

---
+++ Hackerrank/CaesarsCipher.swift
@@ -0,0 +1,49 @@
+//
+//  CeasarsCipher.swift
+//  Hackerrank
+//
+//  Created by Sebastian Tleye on 18/01/2024.
+//
+
+import Foundation
+
+struct CaesarsCipher {
+
+    func solve() {
+        print(self.caesarCipher(s: "There's a starman waiting in the Sky", k: 3))
+    }
+
+    func caesarCipher(s: String, k: Int) -> String {
+        var result = ""
+        for char in s {
+            result += String(cipher(char, k: k))
+        }
+        return result
+    }
+
+    func cipher(_ c: Character, k: Int) -> Character {
+        var c = c
+        let isUppercase = c.isUppercase
+        if isUppercase {
+            c = Character(c.lowercased())
+        }
+        let alphabet = "abcdefghijklmnopqrstuvwxyz"
+        if let firstIndex = alphabet.firstIndex(of: c) {
+            let index = alphabet.distance(from: alphabet.startIndex, to: firstIndex)
+            let newIndex = (index + k) % 26
+
+            return isUppercase ? Character(alphabet[newIndex].uppercased()) : alphabet[newIndex]
+        }
+        return c
+    }
+
+}
+
+extension String {
+    
+    subscript(index: Int) -> Character {
+        let charIndex = self.index(startIndex, offsetBy: index)
+        return self[charIndex]
+    }
+
+}
