42752387a3beba41f0283b5fa0ccf70a5aa1a596
Sebastian Tleye <stleye@gmail.com>
2024-01-24 22:24:27 -0300

    Added BirthdayCakeCandles

---
+++ Hackerrank/BirthdayCakeCandles.swift
@@ -0,0 +1,34 @@
+//
+//  BirthdayCakeCandles.swift
+//  Hackerrank
+//
+//  Created by Sebastian Tleye on 24/01/2024.
+//
+
+import Foundation
+
+struct BirthdayCakeCandles {
+    
+    func solve() {
+        print(birthdayCakeCandles(candles: [4, 4, 1, 3]))
+    }
+    
+    func birthdayCakeCandles(candles: [Int]) -> Int {
+        
+        var total = 0
+        var max = candles[0]
+
+        for candle in candles {
+            if candle > max {
+                max = candle
+                total = 1
+            } else if candle == max {
+                total += 1
+            }
+        }
+        
+        return total
+
+    }
+    
+}
