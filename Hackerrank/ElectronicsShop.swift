d6d11a0ab4cc11479b3ca64c8f59e8453404c7bc
Sebastian Tleye <stleye@gmail.com>
2024-01-30 09:12:10 -0300

    Added ElectronicsShop

---
+++ Hackerrank/ElectronicsShop.swift
@@ -0,0 +1,29 @@
+//
+//  ElectronicsShop.swift
+//  Hackerrank
+//
+//  Created by Sebastian Tleye on 30/01/2024.
+//
+
+import Foundation
+
+struct ElectronicsShop {
+
+    func solve() {
+        getMoneySpent(keyboards: [40, 50, 60], drives: [5, 8, 12], b: 60)
+    }
+
+    func getMoneySpent(keyboards: [Int], drives: [Int], b: Int) -> Int {
+        var max = -1
+        for keyboard in keyboards {
+            for drive in drives {
+                let sum = keyboard + drive
+                if sum <= b && sum > max {
+                    max = sum
+                }
+            }
+        }
+        return max
+    }
+
+}
