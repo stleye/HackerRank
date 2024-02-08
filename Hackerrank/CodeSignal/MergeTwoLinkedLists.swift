1dfca1a03a92f9131ecc0283dd6f943ba5ea27e8
Sebastian Tleye <stleye@gmail.com>
2024-02-08 07:56:36 -0300

    Improved solution for MergeTwoLinkedLists

--- Hackerrank/CodeSignal/MergeTwoLinkedLists.swift
+++ Hackerrank/CodeSignal/MergeTwoLinkedLists.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct MergeTwoLinkedLists {
-    
+
     func solve() {
         let a = ListNode(1)
         a.next = ListNode(1)
@@ -19,39 +19,21 @@ struct MergeTwoLinkedLists {
         b.next = ListNode(3)
         b.next?.next = ListNode(5)
 
-        solution(l1: a, l2: nil)
+        var r = solution(l1: a, l2: b)
+        print(r)
     }
     
     func solution(l1: ListNode<Int>?, l2: ListNode<Int>?) -> ListNode<Int>? {
-        guard l1 != nil || l2 != nil else { return nil }
-        
-        var r: ListNode<Int>? = ListNode(0)
-        var il1 = l1
-        var il2 = l2
-        var ir = r
+        guard let l1 = l1 else { return l2 }
+        guard let l2 = l2 else { return l1 }
 
-        while il1 != nil || il2 != nil {
-            if il1 != nil && il2 != nil {
-                if il1!.value < il2!.value {
-                    ir?.next = ListNode(il1!.value)
-                    il1 = il1!.next
-                } else {
-                    ir?.next = ListNode(il2!.value)
-                    il2 = il2!.next
-                }
-            } else if il1 != nil  {
-                ir?.next = ListNode(il1!.value)
-                il1 = il1!.next
-            } else {
-                ir?.next = ListNode(il2!.value)
-                il2 = il2!.next
-            }
-            ir = ir?.next
+        if l1.value < l2.value {
+            l1.next = solution(l1: l1.next, l2: l2)
+            return l1
+        } else {
+            l2.next = solution(l1: l1, l2: l2.next)
+            return l2
         }
-
-        r = r?.next
-
-        return r
     }
     
     public class ListNode<T> {
