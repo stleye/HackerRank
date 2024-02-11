3f9c7555607b5d8d6e41e5acee0d4b4aa13b7d8f
Sebastian Tleye <stleye@gmail.com>
2024-02-10 21:20:57 -0300

    Added SortByHeight

---
+++ Hackerrank/CodeSignal/SortByHeight.swift
@@ -0,0 +1,30 @@
+//
+//  SortByHeight.swift
+//  Hackerrank
+//
+//  Created by Sebastian Tleye on 10/02/2024.
+//
+
+import Foundation
+
+struct SortByHeight {
+    
+    func solve() {
+        let a = [-1, 150, 190, 170, -1, -1, 160, 180]
+        print(solution(a: a))
+    }
+    
+    func solution(a: [Int]) -> [Int] {
+        let r = a.filter { $0 != -1 }.sorted()
+        var res = a
+        var j = 0
+        for i in 0..<res.count {
+            if res[i] != -1 {
+                res[i] = r[j]
+                j += 1
+            }
+        }
+        return res
+    }
+    
+}
