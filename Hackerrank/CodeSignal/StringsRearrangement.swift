0341569894a21a9651ad3e7ad0afc82704ba9f50
Sebastian Tleye <stleye@gmail.com>
2024-03-02 12:53:15 -0300

    Added FormingAMagicSquare

--- Hackerrank/CodeSignal/StringsRearrangement.swift
+++ Hackerrank/CodeSignal/StringsRearrangement.swift
@@ -12,11 +12,41 @@ import Foundation
 struct StringsRearrangement {
     
     func solve() {
-        print(solution(inputArray: ["aba", "bbb", "bab"]))
+        //print(solution(inputArray: ["aba", "bbb", "bab"]))
+        //print(solution(inputArray: ["ab", "bb", "aa"]))
+        print(solution(inputArray: ["abc", "abx", "axx", "abx", "abc"]))
     }
     
     func solution(inputArray: [String]) -> Bool {
-        return true
+        var result = [[String]]()
+
+        func permute(head: [String], tail: [String]) {
+            if tail.isEmpty {
+                result += [head]
+            }
+            for (i, t) in tail.enumerated() {
+                var newTail = tail
+                newTail.remove(at: i)
+                permute(head: head + [t], tail: newTail)
+            }
+        }
+        permute(head: [], tail: inputArray)
+
+        return result.first { arr in
+            zip(arr, arr[1...]).reduce(true) { $0 && zip($1.0, $1.1).filter { $0 != $1 }.count == 1 }
+        } != nil
     }
+
+
+//    func differsOnlyInOneLetter(_ str1: String, _ str2: String) -> Bool {
+//        var count = 0
+//        for i in 0..<str1.count {
+//            if str1[i] != str2[i] {
+//                count += 1
+//                if count > 1 { return false }
+//            }
+//        }
+//        return count == 1
+//    }
     
 }
--- Hackerrank/HackerRank/FormingAMagicSquare.swift
+++ Hackerrank/HackerRank/FormingAMagicSquare.swift
@@ -12,11 +12,62 @@ import Foundation
 struct FormingAMagicSquare {
     
     func solve() {
-        
+        print(formingMagicSquare(s: [[4, 8, 2], [4, 5, 7], [6, 1, 6]]))
     }
     
     func formingMagicSquare(s: [[Int]]) -> Int {
-        return 0
+        let allMagicSquares =
+            [[
+                [8, 1, 6],
+                [3, 5, 7],
+                [4, 9, 2]],
+             [
+                [4, 3, 8],
+                [9, 5, 1],
+                [2, 7, 6]],
+             [
+                [2, 9, 4],
+                [7, 5, 3],
+                [6, 1, 8]],
+             [
+                [6, 7, 2],
+                [1, 5, 9],
+                [8, 3, 4]],
+             [
+                [6, 1, 8],
+                [7, 5, 3],
+                [2, 9, 4]],
+             [
+                [8, 3, 4],
+                [1, 5, 9],
+                [6, 7, 2]],
+             [
+                [4, 9, 2],
+                [3, 5, 7],
+                [8, 1, 6]],
+             [
+                [2, 7, 6],
+                [9, 5, 1],
+                [4, 3, 8]]]
+
+        var min = Int.max
+        for magicSquare in allMagicSquares {
+            let costFromSToMagicSquare = cost(from: s, to: magicSquare)
+            if costFromSToMagicSquare < min {
+                min = costFromSToMagicSquare
+            }
+        }
+        return min
+    }
+    
+    func cost(from m1: [[Int]], to m2: [[Int]]) -> Int {
+        var cost = 0
+        for i in 0..<3 {
+            for j in 0..<3 {
+                cost += abs(m1[i][j] - m2[i][j])
+            }
+        }
+        return cost
     }
 
 }
