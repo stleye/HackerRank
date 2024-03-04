0b4674a3512bae82300fec998cfc65236c06834f
Sebastian Tleye <stleye@gmail.com>
2024-03-04 07:41:37 -0300

    Added JumpingOnClouds

--- Hackerrank/HackerRank/JumpingOnClouds.swift
+++ Hackerrank/HackerRank/JumpingOnClouds.swift
@@ -12,11 +12,22 @@ import Foundation
 struct JumpingOnClouds {
     
     func solve() {
-        print(jumpingOnClouds(c: [0, 0, 1, 0], k: 2))
+        print(jumpingOnClouds(c: [1, 1, 1, 0, 1, 1, 0, 0, 0, 0], k: 3))
     }
 
     func jumpingOnClouds(c: [Int], k: Int) -> Int {
-        return 0
+        var energy = 100
+        var currentCloud = 0
+        let n = c.count
+        repeat {
+            energy -= 1
+            if c[currentCloud] == 1 {
+                energy -= 2
+            }
+            currentCloud = (currentCloud + k) % n
+        } while (currentCloud % n) != 0
+
+        return energy
     }
 
 }
