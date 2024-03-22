848dadeb4baba9a6360877e98cb8a70b86addd86
Sebastian Tleye <stleye@gmail.com>
2024-03-22 13:03:16 -0300

    Added Grep_1 Grep_2 Grep_3

---
+++ Bash/Grep_1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#https://www.hackerrank.com/challenges/text-processing-in-linux-the-grep-command-1/problem
+
+grep -E "\b(the)\b" $1
---
+++ Bash/Grep_2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#https://www.hackerrank.com/challenges/text-processing-in-linux-the-grep-command-2/problem
+
+grep -i -E "\b(the)\b" $1
