066eef160e737093d4a147ab1b41e7192d4edb61
Sebastian Tleye <stleye@gmail.com>
2025-08-04 14:18:29 -0300

    Added ManasaAndStones

--- Hackerrank/HackerRank/ManasaAndStones.swift
+++ Hackerrank/HackerRank/ManasaAndStones.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+//https://www.hackerrank.com/challenges/manasa-and-stones
+
+struct ManasaAndStones {
+    
+    func solve() {
+        print(stones(n: 4, a: 10, b: 100))
+    }
+    
+    func stones(n: Int, a: Int, b: Int) -> [Int] {
+        var stepI: [Int] = [0]
+        for _ in 1...n-1 {
+            var nextStep: [Int: Int] = [:]
+            for k in stepI {
+                nextStep[k+a] = (nextStep[k+a] ?? 0) + 1
+                nextStep[k+b] = (nextStep[k+b] ?? 0) + 1
+            }
+            stepI = Array(nextStep.keys)
+        }
+        return stepI.sorted()
+    }
+    
+}
