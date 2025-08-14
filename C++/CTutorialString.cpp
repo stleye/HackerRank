89994aa826362f13bb90148540e36df2080bc11e
Sebastian Tleye <stleye@gmail.com>
2025-08-14 17:03:48 -0300

    More on C++ Exercises

---
+++ C++/CTutorialString.cpp
@@ -0,0 +1,19 @@
+//https://www.hackerrank.com/challenges/c-tutorial-strings/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string a, b;
+	
+	cin >> a;
+	cin >> b;
+
+	cout << a.size() << " " << b.size() << endl;
+	cout << a << b << endl;
+	cout << b[0] << a.substr(1) << " " << a[0] << b.substr(1) << endl;
+
+    return 0;
+}
