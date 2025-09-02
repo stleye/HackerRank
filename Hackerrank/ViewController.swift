1f88e91a07ee90ae4725201d6ce096fb02c04c4c
Sebastian Tleye <stleye@gmail.com>
2025-09-02 14:14:34 -0300

    Added NonDivisibleSubset

--- Hackerrank/ViewController.swift
+++ Hackerrank/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        ManasaAndStones().solve()
+        NonDivisibleSubset().solve()
 
     }
 
