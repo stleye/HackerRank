48e98c6e1fc3a7504513182af2e5f9fe43000193
Sebastian Tleye <stleye@gmail.com>
2025-09-05 20:42:59 -0300

    Added list comprehension

---
+++ Python/list_comprehensions.py
@@ -0,0 +1,15 @@
+#https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    coordinates = [[a, b, c] for a in range(x+1) 
+    						 for b in range(y+1)
+                  			 for c in range(z+1)
+					if a + b + c != n
+                  ]
+
+    print(coordinates)
